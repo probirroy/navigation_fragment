429d9700cd47548cb5d65a5ae7842b9978752968
probirroy <probirroy2@gmail.com>
2023-11-16 15:08:56 +0600

    Initial commit

---
+++ lib/fragments/first_fragment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FirstFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new Text("Hello Fragment 1",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold)),
+    );
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:navigation_fragment/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'NavigationDrawer Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new HomePage(),
+    );
+  }
+}
